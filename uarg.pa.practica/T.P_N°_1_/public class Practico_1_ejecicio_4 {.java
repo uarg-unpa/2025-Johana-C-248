0bf9beff8336f98e79ea1f782abefef43dc04172
Johana Cerone <j.cerone.je@gmail.com>
2025-09-02 20:19:29 +0000

    actualizo

---
+++ uarg.pa.practica/T.P_N°_1_/public class Practico_1_ejecicio_4 {.java
@@ -0,0 +1,9 @@
+public class Practico_1_ejecicio_4 {
+    public static void main (String[] args) {
+        System.out.println("Ingrese 3 números enteros: ");
+        int NUMRO1, NUMRO2, NUMRO3;
+        if (NUMRO > NUMRO2 & NUMRO1 > NUMRO3) {
+            system.out.println("ccccc")
+        }
+
+}
