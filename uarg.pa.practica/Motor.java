95796455a5baa13c8a22635b750fd85344aa5358
Johana Cerone <j.cerone.je@gmail.com>
2025-09-15 17:40:24 +0000

    actualizar

--- uarg.pa.practica/Motor.java
+++ uarg.pa.practica/Motor.java
@@ -5,6 +5,29 @@ public class Motor {
     public String marca;
     public int serie;
 
-public     
+    public Motor(int cilindrada, String tipo, char modelo, String marca, int serie) {
+        this.cilindrada = cilindrada;
+        this.tipo = tipo;
+        this.modelo = modelo;
+        this.marca = marca;
+        this.serie = serie;
+    }
+
+    public void setCilindrada(int cilindrada) {
+        this.cilindrada = cilindrada;
+    }
+    
+   public int getCilindrada() {
+        return cilindrada;
+   }
+
+   public void setTipo(String tipo) {
+       this.tipo = tipo;
+   }
+
+   public String getTipo() {
+       return 
+   }
+}     
 
 }  
